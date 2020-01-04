b9aa8c2b7cf3cf84081fcf6027d90a2e12ee1d05
unknown <yanch22@gmail.com>
2020-01-04 19:38:05 +0200

    Update file created function that connects to sql server and creadtes db with columes

--- Queries.py
+++ Queries.py
@@ -1,12 +1,34 @@
 import mysql.connector
+from sqlite3 import OperationalError
+
 
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
     passwd="Aa123456",
-    database="funny_name"
+    allow_local_infile=True
 )
 mycursor = mydb.cursor()
+
+
+def execute_scripts_from_file(filename):
+    # Open and read the file as a single buffer
+    fd = open(filename, 'r')
+    sql_file = fd.read()
+    fd.close()
+
+    # all SQL commands (split on ';')- no need for ";"
+    sqlCommands = sql_file.split(';')
+
+    # Execute every command from the input file
+    for command in sqlCommands:
+        # This will skip and report errors
+        # For example, if the tables do not yet exist, this will skip over
+        # the DROP TABLE commands
+        try:
+            mycursor.execute(command)
+        except OperationalError as msg:
+            print("Command skipped: " + msg)
 
 
 def get_info_by_command(command_string):
@@ -33,6 +55,7 @@ def try_command():
     return get_info_by_command(cmd)
 
 def main():
+    execute_scripts_from_file("build_tables.sql")
    # get_artist("Adele")
     res = try_command()
     print(res)
---
+++ build_tables.sql
@@ -0,0 +1,98 @@
+
+ BEGIN;
+
+CREATE USER IF NOT EXISTS 'funny_name'@'localhost' IDENTIFIED BY 'funny_name';
+CREATE DATABASE IF NOT EXISTS funny_name;
+GRANT ALL PRIVILEGES ON funny_name.* TO 'funny_name'@'localhost' WITH GRANT OPTION;
+GRANT FILE ON *.* to 'funny_name'@'localhost';
+USE funny_name;
+
+CREATE TABLE IF NOT EXISTS albums ( -- replicate
+    id                      BIGINT UNSIGNED, -- PK
+    name                    VARCHAR(255),
+    artist_credit           INTEGER,
+    platform                VARCHAR(64) NOT NULL,
+    type                VARCHAR(255) NOT NULL,
+    first_release_date_year   SMALLINT
+) CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+
+CREATE TABLE IF NOT EXISTS artist_to_credit ( -- replicate (verbose)
+    artist              INTEGER NOT NULL, -- references
+    artist_credit       INTEGER NOT NULL -- PK
+) CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+
+CREATE TABLE IF NOT EXISTS artist ( -- replicate (verbose)
+    id                  BIGINT UNSIGNED,
+    name                VARCHAR(255) NOT NULL,
+	gender              VARCHAR(255) NOT NULL,
+    area                VARCHAR(255) NOT NULL,
+    type                VARCHAR(255) NOT NULL,
+    begin_date_year     SMALLINT,
+    begin_date_month    SMALLINT,
+    begin_date_day      SMALLINT
+) CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+CREATE TABLE IF NOT EXISTS songs ( -- replicate (verbose)
+    id                  BIGINT UNSIGNED, -- PK 
+    name                VARCHAR(255) NOT NULL,
+    artist_credit       INTEGER NOT NULL, 
+    medium              INTEGER NOT NULL 
+) CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+CREATE TABLE IF NOT EXISTS genres ( -- replicate (verbose)
+    name                VARCHAR(255) NOT NULL
+) CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+CREATE TABLE IF NOT EXISTS albums_genres ( -- replicate (verbose)
+    album_id       INTEGER,
+    genre          VARCHAR(255) NOT NULL
+) CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+
+CREATE TABLE IF NOT EXISTS artist_genres ( -- replicate (verbose)
+    artist_id                  BIGINT UNSIGNED,
+    genre              		   VARCHAR(255) NOT NULL,
+	count         			   INTEGER 
+) CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+CREATE TABLE IF NOT EXISTS mediums ( -- replicate (verbose)
+    id                  BIGINT UNSIGNED,
+    `release`               INTEGER NOT NULL,
+    format               VARCHAR(255) NOT NULL,
+    artist_credit		 INTEGER NOT NULL,
+    release_group		INTEGER NOT NULL,
+    language			 VARCHAR(255) NOT NULL
+) CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+CREATE TABLE IF NOT EXISTS users ( -- replicate (verbose)
+    user_id                   INT NOT NULL AUTO_INCREMENT,
+    username              		   VARCHAR(255) NOT NULL,
+	password         			   VARCHAR(255) NOT NULL ,
+    first_game_points					INT,
+    second_game_point					INT,
+    third_game_points					INT,
+	PRIMARY KEY (user_id)
+) CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+CREATE TABLE IF NOT EXISTS users_preferences ( -- replicate (verbose)
+    user_id                  INT,
+	type  					 VARCHAR(255) NOT NULL,
+    preference               VARCHAR(255) NOT NULL,
+    count                    INT
+) CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+
+LOAD DATA LOCAL INFILE 'C:\\Users\\YanaP\\Desktop\\Bar-Ilan\\FunnyName\\DB_FUNNY_NAME\\ARTIST_TO_CREDIT.csv' IGNORE  INTO TABLE artist_to_credit FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
+LOAD DATA LOCAL INFILE 'C:\\Users\\YanaP\\Desktop\\Bar-Ilan\\FunnyName\\DB_FUNNY_NAME\\ARTISTS.csv' INTO TABLE artist FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
+LOAD DATA LOCAL INFILE 'C:\\Users\\YanaP\\Desktop\\Bar-Ilan\\FunnyName\\DB_FUNNY_NAME\\CD_DATA.csv' INTO TABLE mediums FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
+LOAD DATA LOCAL INFILE 'C:\\Users\\YanaP\\Desktop\\Bar-Ilan\\FunnyName\\DB_FUNNY_NAME\\ARTISTS_GENRE.csv' INTO TABLE artist_genres FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
+LOAD DATA LOCAL INFILE 'C:\\Users\\YanaP\\Desktop\\Bar-Ilan\\FunnyName\\DB_FUNNY_NAME\\GENRES.csv' INTO TABLE genres FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
+LOAD DATA LOCAL INFILE 'C:\\Users\\YanaP\\Desktop\\Bar-Ilan\\FunnyName\\DB_FUNNY_NAME\\ALBUMS.csv' INTO TABLE albums FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
+LOAD DATA LOCAL INFILE 'C:\\Users\\YanaP\\Desktop\\Bar-Ilan\\FunnyName\\DB_FUNNY_NAME\\ALBUMS_GENRE.csv' INTO TABLE albums_genres FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
+LOAD DATA LOCAL INFILE 'C:\\Users\\YanaP\\Desktop\\Bar-Ilan\\FunnyName\\DB_FUNNY_NAME\\SONGS.csv' INTO TABLE songs FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
+
+COMMIT;
+
+-- vi: set ts=4 sw=4 et :
