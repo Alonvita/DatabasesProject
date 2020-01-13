cacd27bd4b491ac41b88515a909733541429b78b
alon.vita@gmail.com <Zebra1Zebra2!>
2020-01-13 16:09:30 +0200

    changes

--- Logic/Authentications.py
+++ Logic/Authentications.py
@@ -1,26 +1,48 @@
-import pandas as pd
-from Logic.User import User
+import Queries
 
 
-class Authenticator:
-    PREFERENCES_EXIST_STATUS = 1
-    PREFERENCES_NON_EXISTING = 0
-    USER_DOESNT_EXIST = -1
-
-    def login(self, username, password):
-        user_data_frame = self.load_user_from_data_base(username, password)
-
-        if user_data_frame is None:
-            return self.USER_DOESNT_EXIST, None
-
-        user = User()  # TODO: Create a new user from the df
-
-        if user.get_preferences_dict():
-            return self.PREFERENCES_EXIST_STATUS, user
-        else:
-            return self.PREFERENCES_NON_EXISTING, user
+PREFERENCES_EXIST_STATUS = 1
+PREFERENCES_NON_EXISTING = 0
+USER_DOESNT_EXIST = -1
+ADD_FAILURE = -1
 
 
-    def load_user_from_data_base(self, username, password):
-        # TODO: question DB if user exists
-        pass
+def login(username, password):
+    user_id = load_user_from_data_base(username, password)
+
+    if user_id == USER_DOESNT_EXIST:
+        return USER_DOESNT_EXIST
+
+    user_preferences = None  # TODO: get user preferences from db
+
+    if user_preferences:
+        return PREFERENCES_EXIST_STATUS
+    else:
+        return PREFERENCES_NON_EXISTING
+
+
+def register_user(user_name, password):
+    """
+    adds a new user to the database
+
+    :param user_name:
+    :param password:
+    :param properties_list:
+    :return: 0 if user exists or 1 if user was created
+    """
+    add_success = 1
+    add_fail = 0
+
+    if Queries.add_user(user_name, password) == ADD_FAILURE:
+        return add_fail
+
+    return add_success
+
+
+def add_properties_to_user(username, properties_list):
+    # TODO: add properties list to user in DB
+    pass
+
+
+def load_user_from_data_base(username, password):
+    return Queries.get_user_id(username, password)
